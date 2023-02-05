db9b838d1bb6461020d102002a46e386167e1803
EvgeniyLyapunov <lyapunovcs@gmail.com>
2023-02-05 23:45:03 +0300

    work over show me-modals

--- app/js/modules/me.js
+++ app/js/modules/me.js
@@ -11,7 +11,22 @@ const me = () => {
     menuBtn.classList.toggle('me__burger_active');
   });
 
+  const meButtonsBlock = document.querySelector('.me__nav-list'),
+        modals = document.querySelectorAll('.modal-me'),
+        overlayMe = document.querySelector('.overlay-me'),
+        closeMeModal = document.querySelectorAll('.modal-me__close');
 
+  function showMeModal(modalSelector) {
+    overlayMe.classList.add('overlay-me_show');
+    overlayMe.classList.add('animation-overlay-show');
+  }
+
+  meButtonsBlock.addEventListener('click', (e) => {
+    if(e.target.classList.contains('button-base')) {
+      showMeModal(e.target.getAttribute('data-modal'));
+    } 
+  });
+  
 }
 
 export default me;
