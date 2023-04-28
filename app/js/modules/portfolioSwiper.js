6e127b0c2557939e99aef77645ef5b134fa9a405
EvgeniyLyapunov <lyapunovcs@gmail.com>
2023-04-28 22:31:53 +0300

    work over content of slider in section portfolio

--- app/js/modules/portfolioSwiper.js
+++ app/js/modules/portfolioSwiper.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper/bundle';
 
 const portfolioSwiper = new Swiper('.portfolio-swiper', {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 100,
   loop: true,
   pagination: {
     el: '.swiper-pagination',
